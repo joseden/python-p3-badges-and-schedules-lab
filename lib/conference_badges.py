0dab85a7e0e8039a809bb7f67b600999535cc617
Joseph <joseph.dena@student.moringaschool.com>
2024-01-19 15:35:38 +0300

    complete assignment

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,27 @@
 def badge_maker(name):
-    return None
+   return f"Hello, my name is {name}."
+name = "Ariel"
+
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badges.append(f"Hello, my name is {name}.")
+    return badges
 
 def assign_rooms(names):
-    return None
+    assignments = []
+    for room in range(1, len(names) + 1):
+        assignments.append(f'Hello, {names[room - 1]}! You\'ll be assigned to room {room}!')
+    
+    return assignments
+
 
 def printer(names):
-    return None
+    for badges in batch_badge_creator(names):
+        print(badges)
+
+    for assignments in assign_rooms(names):
+        print(assignments)
+
+        
